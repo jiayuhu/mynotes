17fc169c6cfd1448bbdccc193534918bb07843e7
Yuhooo <2512541+jiayuhu@users.noreply.github.com>
2025-05-11 22:23:45 +0800

    try to add register page

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,5 +1,5 @@
 import { authentication, createDirectus, rest } from '@directus/sdk';
 
-const client = createDirectus("http://localhost:8055").with(rest()).with(authentication("cookie", { credentials: "include" }));
+const client = createDirectus("https://directus.appliket.com").with(rest()).with(authentication("cookie", { credentials: "include" }));
 
 export default client;
